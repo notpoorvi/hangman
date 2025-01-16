7aa11da73f033210b6751012e7d8a0680d0554a4
Poorvi <poorvi.b26@gmail.com>
2025-01-16 14:35:31 -0800

    drew hangman

---
+++ src/HangmanWord.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const HangmanWord = () => {
+  const word = "test";
+  const guessedLetters = ["t", "e", "g"];
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "5rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+            key={index}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
+};
+
+export default HangmanWord;
---
+++ src/Keyboard.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const KEYS = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+const Keyboard = () => {
+  return <div>Keyboard</div>;
+};
+
+export default Keyboard;
